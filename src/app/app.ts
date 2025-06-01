0002ac60aee9c25332662e33d3092c4d5d9dd320
NicolasJavierJan <nicolasjavierjan@gmail.com>
2025-06-01 18:40:40 +0200

    list of Pokemon now shows

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PokemonService } from './services/pokemon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
   protected title = 'Pokedex';
+  pokemon : any[] = []
+
+  constructor(private PokemonService: PokemonService) {}
+
+  ngOnInit(): void {
+    this.PokemonService.getPokemonList().subscribe(data => {
+      console.log(data);
+      this.pokemon = data.results;
+    })
+  }
 }
