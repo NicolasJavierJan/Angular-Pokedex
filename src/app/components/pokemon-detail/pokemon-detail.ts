533a35b8ff465551112ecab9329d4ef418ebe500
NicolasJavierJan <nicolasjavierjan@gmail.com>
2025-06-06 14:59:32 +0200

    added abilities and descriptions

--- src/app/components/pokemon-detail/pokemon-detail.ts
+++ src/app/components/pokemon-detail/pokemon-detail.ts
@@ -3,10 +3,11 @@ import { PokemonService } from '../../services/pokemon';
 import { CommonModule } from '@angular/common';
 import { PokemonStatsGraph } from '../pokemon-stats-graph/pokemon-stats-graph';
 import { PokemonTypes } from '../pokemon-types/pokemon-types';
+import { PokemonAbilities } from '../pokemon-abilities/pokemon-abilities';
 
 @Component({
   selector: 'app-pokemon-detail',
-  imports: [CommonModule, PokemonStatsGraph, PokemonTypes],
+  imports: [CommonModule, PokemonStatsGraph, PokemonTypes, PokemonAbilities],
   templateUrl: './pokemon-detail.html',
   styleUrls: ['./pokemon-detail.css'],
   standalone: true,
