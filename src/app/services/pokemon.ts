9a2e8a3504d16af0f3c9b365356f17e146f4e272
NicolasJavierJan <nicolasjavierjan@gmail.com>
2025-06-23 17:58:37 +0200

    added two lists of pokemon: pokemon with composed names (hyphenated in api) and pokemon with different forms (also hyphenated). now it searches for the correct ones and does not fail when fetching

--- src/app/services/pokemon.ts
+++ src/app/services/pokemon.ts
@@ -23,15 +23,28 @@ export class PokemonService {
     
     const hyphenated = all.filter((p: any) => p.name.includes('-'));
 
-    const specialCases = ['nidoran-f', 'nidoran-m', ];
+    const nameHasHyphen = ['nidoran-f', 'nidoran-m', 'mr-mime', 'ho-oh', 'mime-jr', 'type-null', 'jangmo-o', 'hakamo-o',
+      'kommo-o', 'tapu-bulu', 'tapu-koko', 'tapu-lele', 'tapu-fini', 'mr-rime', 'great-tusk', 'scream-tail', 'brute-bonnet',
+      'flutter-mane', 'slither-wing', 'sandy-shocks', 'iron-treads', 'iron-bundle', 'iron-hands', 'iron-jugulis', 'iron-moth',
+      'iron-thorns', 'wo-chien', 'chien-pao', 'ting-lu', 'chi-yu', 'roaring-moon', 'iron-valiant', 'iron-leaves', 'walking-wake',
+      'gouging-fire', 'raging-bolt', 'iron-boulder', 'iron-crown' 
+    ];
+
+    const nameHasForm = ['deoxys-normal', 'wormadam-plant', 'giratina-altered', 'shaymin-land', 'basculin-red-striped', 
+      'darmanitan-standard', 'tornadus-incarnate', 'thundurus-incarnate', 'landorus-incarnate', 'keldeo-ordinary', 'meloetta-aria',
+      'meowstic-male', 'aegislash-shield', 'pumpkaboo-average', 'gourgeist-average', 'zygarde-50', 'oricorio-baile', 'lycanroc-midday',
+      'wishiwashi-solo', 'minior-red-meteor', 'mimikyu-disguised', 'toxtricity-amped', 'eiscue-ice', 'indeedee-male', 'morpeko-full-belly',
+      'urshifu-single-strike', 'basculegion-male', 'enamorus-incarnate', 'oinkologne-male', 'maushold-family-of-four', 'squawkabilly-green-plumage',
+      'palafin-zero', 'tatsugiri-curly', 'dudunsparce-two-segment' 
+    ];
 
     for (const p of hyphenated) {
       try {
-        const speciesName = specialCases.includes(p.name)
+        const speciesName = nameHasHyphen.includes(p.name) // If nameHasHyphen then search with hyphen (otherwise it will fail)
           ? p.name
-          : p.name.includes('-')
+          : nameHasForm.includes(p.name) // else, name is a form? If yes, search only the Species name.
           ? p.name.split('-')[0]
-          : p.name;
+          : p.name // Shouldn't reach here.
         
         const species = await firstValueFrom(
           this.http.get<any>(`${this.baseUrl}-species/${speciesName}`)
